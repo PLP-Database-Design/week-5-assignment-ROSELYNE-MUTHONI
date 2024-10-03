68e93c96eb50c544bd80ef70fa0f070d74ab9a1b
ROSELYNE-MUTHONI <rmuthoni317@outlook.com>
2024-10-03 23:55:58 +0300

    updated assignment

--- server.js
+++ server.js
@@ -1,103 +1,105 @@
-//import our dependencies
-const express = require ("express");
-const dotenv= require('dotenv')
-const mysql = require('mysql2')
-
+// Declare dependancies/variables
+const express = require('express')
 const app = express()
-dotenv.config()
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
+const path = require('path');
 
-//basic end point to say hello world
-app.get(``,(req,res) => {
-    res.send('Hello World!')
-})
+dotenv.config();
+app.use(express.json())
+app.set('view engine', 'ejs');
+// app.set('views', __dirname='/views');
+app.set('views', path.join(__dirname, 'views'));
 
-// start and listen to server
 
-const PORT = 3000
-   app.listen(PORT, () => {
+dotenv.config();
+console.log('Database host:', process.env.DB_HOST);
+console.log('Database user:', process.env.DB_USER);
+
+// Create connection
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+  });
+
+  // Test connection
+db.connect((err) => {
+    // Connection not successful
+    if (err) {
+      return console.error('Database connection failed:', err.stack)
+    }
+    // connection is successful
+    console.log('Connected to the database successfully as Id:', db.threadId)
+  })
+  
+  //  A `GET' endpoint and retrieve from Patients
+  app.get('/patients', (req, res) => {
+    const getpatients = "SELECT patient_id, first_name, last_name, date_of_birth FROM patients"
+    db.query(getpatients, (err,data) => {
+        // if I have an error
+      if (err) {
+        return res.status(400).send('Failed to get patients')
+      }
+      else{
+         //Display the records to the browser 
+      res.status(200).render('patients',{patients: data})
+      }
+    })
+  })
+
+  //Retrieve all providers
+  app.get('/providers', (req, res) => {
+    const getproviders = " SELECT first_name, last_name,provider_specialty FROM providers"
+    db.query(getproviders, (err,data) => {
+        // if I have an error
+      if (err) {
+        return res.status(400).send('Failed to get providers')
+      }
+      else{
+         //Display the records to the browser 
+      res.status(200).render('providers',{data: data})
+      }
+    })
+  })
+    
+// Filter patients by First Name
+   app.get('/patients/first_name/:firstName', (req, res) => {
+    const firstName = req.params.firstName
+     const getpatients = 'SELECT * FROM patients WHERE first_name = ?'
+     db.query(getpatients, [firstName], (err, data) => {
+        // if I have an error
+        if (err) {
+            return res.status(400).send('Failed to get firstName from patients')
+        }
+        else{
+         //Display the records to the browser 
+      res.status(200).render('patients_firstName',{patients: data})
+      }
+    })
+  })
+
+  //Retrieve all providers by their specialty
+  app.get('/providers/provider_specialty/:ProviderSpecialty', (req, res) => {
+    const provider_specialty = req.params.ProviderSpecialty; 
+    db.query('SELECT * FROM providers WHERE provider_specialty = ?', [provider_specialty], (err, data) => {
+ // if I have an error
+ if (err) {
+    return res.status(400).send('Failed to get provider by specialty')
+        }
+        else{
+         //Display the records to the browser 
+         res.status(200).render('provider_specialty', { data: data })
+      }
+    })
+  })
+
+
+   // listen to the serverno\
+   const PORT = 3300
+   app.listen(3300, () => {
      console.log(`server is runnig on http://localhost:${PORT}`)
    })
-
-   //create connection object
-   const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-   })
-
-   //test the connection
-   db.connect((err) =>{
-    //connection not successful
-    if(err) {
-      return console.log('Error connecting to MySQL', err)
-    }
-    //connection successful
-    console.log("MYSQL connection successful")
-   })
-
-//get patients
-app.get('/get-patients', (req, res) => {
-  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients';
-  
-  db.query(query, (err, result) => {
-      if (err) {
-          return res.status(500).json(" Failed to fetch patients ")
-      }
-      //get back results
-      res.status(200).json(results);
-  });
-});
-
-
-// Get providers
-app.get('/get-providers', (req, res) => {
-  const query = 'SELECT first_name, last_name, provider_specialty FROM providers';
-  
-  db.query(query, (err, results) => {
-    if (err) {
-      return res.status(500).json("Failed to fetch providers");
-    }
-    // Get back results
-    res.status(200).json(results);
-  });
-});
-
-// Get patients by first name
-app.get('/get-patients-by-name', (req, res) => {
-  const { first_name } = req.query;
-
-  if (!first_name) {
-    return res.status(400).json("First name is required");
-  }
-
-  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients WHERE first_name = ?';
-  
-  db.query(query, [first_name], (err, results) => {
-    if (err) {
-      return res.status(500).json("Failed to fetch patients by name");
-    }
-    // Get back results
-    res.status(200).json(results);
-  });
-});
-
-// Get providers by speciality
-app.get('/get-providers-by-speciality', (req, res) => {
-  const { provider_speciality } = req.query;
-
-  if (!provider_speciality) {
-    return res.status(400).json("Provider speciality is required");
-  }
-
-  const query = 'SELECT first_name, last_name, provider_specialty FROM providers WHERE provider_speciality = ?';
-  
-  db.query(query, [provider_speciality], (err, results) => {
-    if (err) {
-      return res.status(500).json("Failed to fetch providers by speciality");
-    }
-    // Get back results
-    res.status(200).json(results);
-  });
-});
 
